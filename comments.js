0489e1f17f0add53132c99f3918ba89c43a0231d
Ramón Ballesteros <88846026+RBalles@users.noreply.github.com>
2023-11-23 16:25:48 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,13 +1,19 @@
-// Create a web server
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!');
-});
-
-server.listen(3000, 'localhost', () => {
-    console.log('Server running at http://localhost:3000/');
-});
-
+// Create a web
+// 1. Import express and create a router
+const express = require('express');
+const router = express.Router();
+// 2. Import the controller
+const commentsController = require('../controllers/commentsController');
+// 3. Create the routes
+// 3.1. GET
+router.get('/', commentsController.list);
+// 3.2. POST
+router.post('/', commentsController.create);
+// 3.3. GET
+router.get('/:id', commentsController.detail);
+// 3.4. PUT
+router.put('/:id', commentsController.update);
+// 3.5. DELETE
+router.delete('/:id', commentsController.destroy);
+// 4. Export router
+module.exports = router;
